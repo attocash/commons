1c3a37f2f2b9a73624975d613a50ac1853210ffb
Felipe <felipe@rotilho.com>
2024-02-17 15:49:40 +0100

    Fix tests

--- src/test/kotlin/cash/atto/commons/AttoAccountTest.kt
+++ src/test/kotlin/cash/atto/commons/AttoAccountTest.kt
@@ -2,6 +2,7 @@ package cash.atto.commons
 
 import cash.atto.commons.serialiazers.json.AttoJson
 import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import kotlinx.serialization.encodeToString
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -19,7 +20,7 @@ class AttoAccountTest {
             height = 1U,
             balance = AttoAmount.MAX,
             lastTransactionHash = AttoHash(Random.Default.nextBytes(32)),
-            lastTransactionTimestamp = Clock.System.now(),
+            lastTransactionTimestamp = Instant.fromEpochMilliseconds(Clock.System.now().toEpochMilliseconds()),
             representative = AttoPublicKey(Random.Default.nextBytes(32)),
         )
 
@@ -35,7 +36,7 @@ class AttoAccountTest {
     fun `should deserialize json`() {
         // given
         val expectedJson =
-            """{"publicKey":"45B3B58C26181580EEAFC1791046D54EEC2854BF550A211E2362761077D6590C","version":0,"algorithm":"V1","height":1,"balance":18000000000000000000,"lastTransactionHash":"70F9406609BCB2E3E18F22BD0839C95E5540E95489DC6F24DBF6A1F7CFD83A92","lastTransactionTimestamp":"2024-01-17T18:45:57.478253100Z","representative":"99E439410A4DDD2A3A8D0B667C7A090286B8553378CF3C7AA806C3E60B6C4CBE"}"""
+            """{"publicKey":"45B3B58C26181580EEAFC1791046D54EEC2854BF550A211E2362761077D6590C","version":0,"algorithm":"V1","height":1,"balance":18000000000000000000,"lastTransactionHash":"70F9406609BCB2E3E18F22BD0839C95E5540E95489DC6F24DBF6A1F7CFD83A92","lastTransactionTimestamp":1705517157478,"representative":"99E439410A4DDD2A3A8D0B667C7A090286B8553378CF3C7AA806C3E60B6C4CBE"}"""
 
         // when
         val account = AttoJson.decodeFromString<AttoAccount>(expectedJson)
