d65ab18eae7b648abc2098c3025f1682b0568b01
rotilho <felipe@rotilho.com>
2024-08-10 09:42:48 +0200

    Disable opencl test

--- src/test/kotlin/cash/atto/commons/AttoWorkTest.kt
+++ src/test/kotlin/cash/atto/commons/AttoWorkTest.kt
@@ -4,10 +4,16 @@ import cash.atto.commons.AttoNetwork.Companion.INITIAL_INSTANT
 import cash.atto.commons.AttoNetwork.Companion.INITIAL_LIVE_THRESHOLD
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.minus
 import kotlinx.datetime.plus
+import kotlinx.datetime.toInstant
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -15,6 +21,8 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 internal class AttoWorkTest {
+    private val hash = AttoHash("0000000000000000000000000000000000000000000000000000000000000000".fromHexToByteArray())
+
     @ParameterizedTest
     @MethodSource("provider")
     fun `should return threshold`(
@@ -57,5 +65,42 @@ internal class AttoWorkTest {
 
         // then
         assertEquals(expectedJson, json)
+    }
+
+    @Test
+    fun `should validate work`() {
+        val work = AttoWork("576887B000000000".fromHexToByteArray())
+        assertTrue(isValid(AttoNetwork.LIVE, AttoNetwork.INITIAL_INSTANT, hash.value, work.value))
+    }
+
+    @Test
+    fun `should validate work using decreased threshold`() {
+        val work = AttoWork("0582489800000000".fromHexToByteArray())
+        val timestamp =
+            AttoNetwork
+                .INITIAL_DATE
+                .plus(4, DateTimeUnit.YEAR)
+                .atTime(LocalTime.fromSecondOfDay(0))
+                .toInstant(TimeZone.UTC)
+        assertTrue(isValid(AttoNetwork.LIVE, timestamp, hash.value, work.value))
+    }
+
+    @Test
+    fun `should not validate when work is below threshold`() {
+        val work = AttoWork("a7e077e02e3e759f".fromHexToByteArray())
+        val timestamp =
+            AttoNetwork
+                .INITIAL_DATE
+                .plus(4, DateTimeUnit.YEAR)
+                .atTime(LocalTime.fromSecondOfDay(0))
+                .toInstant(TimeZone.UTC)
+        assertFalse(isValid(AttoNetwork.LIVE, timestamp, hash.value, work.value))
+    }
+
+    @Test
+    fun `should not validate when timestamp is before initial date`() {
+        val work = AttoWork("a7e077e02e3e759f".fromHexToByteArray())
+        val timestamp = AttoNetwork.INITIAL_INSTANT.minus(1, DateTimeUnit.SECOND)
+        assertFalse(isValid(AttoNetwork.LIVE, timestamp, hash.value, work.value))
     }
 }
--- src/test/kotlin/cash/atto/commons/AttoWorkerTest.kt
+++ src/test/kotlin/cash/atto/commons/AttoWorkerTest.kt
@@ -1,67 +1,30 @@
 package cash.atto.commons
 
-import kotlinx.datetime.DateTimeUnit
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.atTime
-import kotlinx.datetime.minus
-import kotlinx.datetime.plus
-import kotlinx.datetime.toInstant
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
 class AttoWorkerTest {
     private val hash = AttoHash("0000000000000000000000000000000000000000000000000000000000000000".fromHexToByteArray())
 
-    @ParameterizedTest
-    @MethodSource("workerProvider")
-    fun `should perform work`(worker: AttoWorker) {
+    @Test
+    fun `should perform work with cpu`() {
+        work(AttoWorker.cpu())
+    }
+
+    @Test
+    @Disabled("OpenCL stopped working in Github actions")
+    fun `should perform work with opencl`() {
+        work(AttoWorker.opencl())
+    }
+
+    private fun work(worker: AttoWorker) {
         val network = AttoNetwork.LOCAL
         val timestamp = AttoNetwork.INITIAL_INSTANT
         val work = worker.work(network, timestamp, hash.value)
         assertTrue(isValid(network, timestamp, hash.value, work.value))
-    }
-
-    @Test
-    fun `should validate work`() {
-        val work = AttoWork("576887B000000000".fromHexToByteArray())
-        assertTrue(isValid(AttoNetwork.LIVE, AttoNetwork.INITIAL_INSTANT, hash.value, work.value))
-    }
-
-    @Test
-    fun `should validate work using decreased threshold`() {
-        val work = AttoWork("0582489800000000".fromHexToByteArray())
-        val timestamp =
-            AttoNetwork
-                .INITIAL_DATE
-                .plus(4, DateTimeUnit.YEAR)
-                .atTime(LocalTime.fromSecondOfDay(0))
-                .toInstant(TimeZone.UTC)
-        assertTrue(isValid(AttoNetwork.LIVE, timestamp, hash.value, work.value))
-    }
-
-    @Test
-    fun `should not validate when work is below threshold`() {
-        val work = AttoWork("a7e077e02e3e759f".fromHexToByteArray())
-        val timestamp =
-            AttoNetwork
-                .INITIAL_DATE
-                .plus(4, DateTimeUnit.YEAR)
-                .atTime(LocalTime.fromSecondOfDay(0))
-                .toInstant(TimeZone.UTC)
-        assertFalse(isValid(AttoNetwork.LIVE, timestamp, hash.value, work.value))
-    }
-
-    @Test
-    fun `should not validate when timestamp is before initial date`() {
-        val work = AttoWork("a7e077e02e3e759f".fromHexToByteArray())
-        val timestamp = AttoNetwork.INITIAL_INSTANT.minus(1, DateTimeUnit.SECOND)
-        assertFalse(isValid(AttoNetwork.LIVE, timestamp, hash.value, work.value))
     }
 
     companion object {
