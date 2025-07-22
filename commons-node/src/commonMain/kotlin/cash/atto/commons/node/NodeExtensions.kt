de878befd4a2a724221a0d6bb9984a66427ddfd9
rotilho <felipe@rotilho.com>
2025-07-22 11:44:37 +0200

    Do not export Receivable new functions to wasm

--- commons-node/src/commonMain/kotlin/cash/atto/commons/node/NodeExtensions.kt
+++ commons-node/src/commonMain/kotlin/cash/atto/commons/node/NodeExtensions.kt
@@ -3,15 +3,16 @@
 package cash.atto.commons.node
 
 import cash.atto.commons.AttoReceivable
+import cash.atto.commons.utils.JsExportForJs
 import kotlinx.serialization.json.Json
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 import kotlin.js.JsName
 
-@JsExport
+@JsExportForJs
 @JsName("receivableToJson")
 fun AttoReceivable.toJson(): String = Json.encodeToString(this)
 
-@JsExport
+@JsExportForJs
 @JsName("receivableFromJson")
 fun String.toReceivable(): AttoReceivable = Json.decodeFromString<AttoReceivable>(this)
