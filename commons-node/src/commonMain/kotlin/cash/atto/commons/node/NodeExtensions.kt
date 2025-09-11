f132dcbb6c096dd33d82432a2c7a02ed81954cd2
rotilho <felipe@rotilho.com>
2025-09-11 11:56:20 +0200

    Add JSON serialization extensions for AttoTransaction and AttoBlock

--- commons-node/src/commonMain/kotlin/cash/atto/commons/node/NodeExtensions.kt
+++ commons-node/src/commonMain/kotlin/cash/atto/commons/node/NodeExtensions.kt
@@ -2,7 +2,10 @@
 
 package cash.atto.commons.node
 
+import cash.atto.commons.AttoBlock
 import cash.atto.commons.AttoReceivable
+import cash.atto.commons.AttoTransaction
+import cash.atto.commons.toBuffer
 import cash.atto.commons.utils.JsExportForJs
 import kotlinx.serialization.json.Json
 import kotlin.js.ExperimentalJsExport
@@ -15,3 +18,36 @@ fun AttoReceivable.toJson(): String = Json.encodeToString(this)
 @JsExportForJs
 @JsName("receivableFromJson")
 fun String.toReceivable(): AttoReceivable = Json.decodeFromString<AttoReceivable>(this)
+
+@JsExportForJs
+@JsName("transactionToJson")
+fun AttoTransaction.toJson(): String = Json.encodeToString(this)
+
+@JsExportForJs
+@JsName("transactionFromJson")
+fun String.toTransaction(): AttoTransaction = Json.decodeFromString<AttoTransaction>(this)
+
+@JsExportForJs
+@JsName("blockToJson")
+fun AttoBlock.toJson(): String = Json.encodeToString(this)
+
+@JsExportForJs
+@JsName("blockFromJson")
+fun String.toBlock(): AttoBlock = Json.decodeFromString<AttoBlock>(this)
+
+@JsExportForJs
+@JsName("fromByteArrayToTransactionJson")
+fun ByteArray.toTransactionJson(): String {
+    val transaction = AttoTransaction.fromBuffer(this.toBuffer()) ?: throw IllegalArgumentException("Invalid transaction")
+    return Json.encodeToString<AttoTransaction>(transaction)
+}
+
+@JsExportForJs
+@JsName("fromByteArrayToBlockJson")
+fun ByteArray.toBlockJson(): String {
+    val block = AttoBlock.fromBuffer(this.toBuffer()) ?: throw IllegalArgumentException("Invalid block")
+    return Json.encodeToString<AttoBlock>(block)
+}
+
+
+
