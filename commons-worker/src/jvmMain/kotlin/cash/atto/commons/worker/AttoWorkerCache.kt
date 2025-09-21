738c82684214d053e87b59c5aaca7e5ccfe85634
rotilho <felipe@rotilho.com>
2025-09-21 15:40:11 +0200

    Enhance AttoWorkerCache to prefer in-progress work computations over direct worker calls

--- commons-worker/src/jvmMain/kotlin/cash/atto/commons/worker/AttoWorkerCache.kt
+++ commons-worker/src/jvmMain/kotlin/cash/atto/commons/worker/AttoWorkerCache.kt
@@ -5,10 +5,11 @@ import cash.atto.commons.AttoWork
 import cash.atto.commons.getThreshold
 import cash.atto.commons.toHex
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import java.util.concurrent.ConcurrentHashMap
 
@@ -17,34 +18,49 @@ private class AttoWorkerCache(
 ) : AttoWorker {
     private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
-    private val cache = ConcurrentHashMap<Pair<ULong, String>, AttoWork>()
+    private val cache = ConcurrentHashMap<Pair<ULong, String>, Deferred<AttoWork>>()
+
+    private fun delegateWork(
+        threshold: ULong,
+        target: ByteArray,
+    ): Deferred<AttoWork> {
+        val key = threshold to target.toHex()
+
+        return cache.computeIfAbsent(key) {
+            scope.async {
+                try {
+                    return@async delegate.work(threshold, target)
+                } catch (e: Exception) {
+                    cache.remove(threshold to target.toHex())
+                    throw e
+                }
+            }
+        }
+    }
 
     override suspend fun work(
         threshold: ULong,
         target: ByteArray,
     ): AttoWork {
-        val work = cache.remove(threshold to target.toHex())
-        if (work != null) {
-            return work
+        val key = threshold to target.toHex()
+
+        val deferred = delegateWork(threshold, target)
+
+        return try {
+            deferred.await()
+        } finally {
+            cache.remove(key, deferred)
         }
-        return delegate.work(threshold, target)
     }
 
     override suspend fun work(block: AttoBlock): AttoWork {
         val work = super.work(block)
-        scope.launch {
-            try {
-                val nextThreshold = AttoWork.getThreshold(block.network, Clock.System.now())
-                val nextTarget = block.hash.value
-                val key = nextThreshold to nextTarget.toHex()
-                if (!cache.containsKey(key)) {
-                    val preComputed = delegate.work(nextThreshold, nextTarget)
-                    cache.putIfAbsent(key, preComputed)
-                }
-            } catch (_: Throwable) {
-                println("Failed to cache pre computed work for $block")
-            }
-        }
+
+        val nextThreshold = AttoWork.getThreshold(block.network, Clock.System.now())
+        val nextTarget = block.hash.value
+
+        delegateWork(nextThreshold, nextTarget)
+
         return work
     }
 
