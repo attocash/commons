6fdf57041f223f2647197fe8cec1e4267e239b79
rotilho <felipe@rotilho.com>
2025-09-27 09:46:03 +0200

    Add extension function to convert `AttoInstant` to `Instant` with Kotlin time interop

--- commons-core/src/jvmMain/kotlin/cash/atto/commons/AttoInstant.jvm.kt
+++ commons-core/src/jvmMain/kotlin/cash/atto/commons/AttoInstant.jvm.kt
@@ -1,5 +1,10 @@
 package cash.atto.commons
 
 import java.time.Instant
+import kotlin.time.ExperimentalTime
+import kotlin.time.toJavaInstant
 
 fun Instant.toAtto(): AttoInstant = AttoInstant.fromEpochMilliseconds(this.toEpochMilli())
+
+@OptIn(ExperimentalTime::class)
+fun AttoInstant.toJavaInstant(): Instant = this.value.toJavaInstant()
