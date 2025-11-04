1ba28e444d55a6254d3e181ade12d94ae2373c0d
rotilho <felipe@rotilho.com>
2025-11-04 21:00:42 +0100

    Fix `heightProvider` in entry and transaction builder

--- commons-node/src/webMain/kotlin/cash/atto/commons/node/monitor/AttoAccountEntryMonitorAsyncBuilder.web.kt
+++ commons-node/src/webMain/kotlin/cash/atto/commons/node/monitor/AttoAccountEntryMonitorAsyncBuilder.web.kt
@@ -2,18 +2,20 @@ package cash.atto.commons.node.monitor
 
 import cash.atto.commons.AttoAddress
 import cash.atto.commons.AttoHeight
+import cash.atto.commons.node.AttoFuture
 import cash.atto.commons.node.AttoNodeClientAsync
+import cash.atto.commons.node.await
 import kotlinx.coroutines.Dispatchers
 
 actual class AttoAccountEntryMonitorAsyncBuilder actual constructor(
     private val nodeClient: AttoNodeClientAsync,
     private val accountMonitor: AttoAccountMonitorAsync,
 ) {
-    private var heightProvider: (AttoAddress) -> AttoHeight = { AttoHeight.MIN }
+    private var heightProvider: suspend (AttoAddress) -> AttoHeight = { AttoHeight.MIN }
 
-    actual fun heightProvider(value: (AttoAddress) -> AttoHeight): AttoAccountEntryMonitorAsyncBuilder =
+    actual fun heightProvider(value: (AttoAddress) -> AttoFuture<AttoHeight>): AttoAccountEntryMonitorAsyncBuilder =
         apply {
-            heightProvider = value
+            heightProvider = { value.invoke(it).await() }
         }
 
     actual fun build(): AttoAccountEntryMonitorAsync {
--- commons-node/src/webMain/kotlin/cash/atto/commons/node/monitor/AttoTransactionMonitorAsyncBuilder.web.kt
+++ commons-node/src/webMain/kotlin/cash/atto/commons/node/monitor/AttoTransactionMonitorAsyncBuilder.web.kt
@@ -2,18 +2,20 @@ package cash.atto.commons.node.monitor
 
 import cash.atto.commons.AttoAddress
 import cash.atto.commons.AttoHeight
+import cash.atto.commons.node.AttoFuture
 import cash.atto.commons.node.AttoNodeClientAsync
+import cash.atto.commons.node.await
 import kotlinx.coroutines.Dispatchers
 
 actual class AttoTransactionMonitorAsyncBuilder actual constructor(
     private val nodeClient: AttoNodeClientAsync,
     private val accountMonitor: AttoAccountMonitorAsync,
 ) {
-    private var heightProvider: (AttoAddress) -> AttoHeight = { AttoHeight.MIN }
+    private var heightProvider: suspend (AttoAddress) -> AttoHeight = { AttoHeight.MIN }
 
-    actual fun heightProvider(value: (AttoAddress) -> AttoHeight): AttoTransactionMonitorAsyncBuilder =
+    actual fun heightProvider(value: (AttoAddress) -> AttoFuture<AttoHeight>): AttoTransactionMonitorAsyncBuilder =
         apply {
-            heightProvider = value
+            heightProvider = { value.invoke(it).await() }
         }
 
     actual fun build(): AttoTransactionMonitorAsync {
