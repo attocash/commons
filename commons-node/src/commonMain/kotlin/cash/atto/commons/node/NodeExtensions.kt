6d91d520d931a5a672769b58f62b7750a38cc85a
rotilho <felipe@rotilho.com>
2025-11-15 22:25:52 +0100

    Add ignore unknown keys to improve compatibility

--- commons-node/src/commonMain/kotlin/cash/atto/commons/node/NodeExtensions.kt
+++ commons-node/src/commonMain/kotlin/cash/atto/commons/node/NodeExtensions.kt
@@ -11,40 +11,45 @@ import kotlinx.serialization.json.Json
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsName
 
+private val json =
+    Json {
+        ignoreUnknownKeys = true
+    }
+
 @JsExportForJs
 @JsName("receivableToJson")
-fun AttoReceivable.toJson(): String = Json.encodeToString(this)
+fun AttoReceivable.toJson(): String = json.encodeToString(this)
 
 @JsExportForJs
 @JsName("receivableFromJson")
-fun String.toReceivable(): AttoReceivable = Json.decodeFromString<AttoReceivable>(this)
+fun String.toReceivable(): AttoReceivable = json.decodeFromString<AttoReceivable>(this)
 
 @JsExportForJs
 @JsName("transactionToJson")
-fun AttoTransaction.toJson(): String = Json.encodeToString(this)
+fun AttoTransaction.toJson(): String = json.encodeToString(this)
 
 @JsExportForJs
 @JsName("transactionFromJson")
-fun String.toTransaction(): AttoTransaction = Json.decodeFromString<AttoTransaction>(this)
+fun String.toTransaction(): AttoTransaction = json.decodeFromString<AttoTransaction>(this)
 
 @JsExportForJs
 @JsName("blockToJson")
-fun AttoBlock.toJson(): String = Json.encodeToString(this)
+fun AttoBlock.toJson(): String = json.encodeToString(this)
 
 @JsExportForJs
 @JsName("blockFromJson")
-fun String.toBlock(): AttoBlock = Json.decodeFromString<AttoBlock>(this)
+fun String.toBlock(): AttoBlock = json.decodeFromString<AttoBlock>(this)
 
 @JsExportForJs
 @JsName("fromByteArrayToTransactionJson")
 fun ByteArray.toTransactionJson(): String {
     val transaction = AttoTransaction.fromBuffer(this.toBuffer()) ?: throw IllegalArgumentException("Invalid transaction")
-    return Json.encodeToString<AttoTransaction>(transaction)
+    return json.encodeToString<AttoTransaction>(transaction)
 }
 
 @JsExportForJs
 @JsName("fromByteArrayToBlockJson")
 fun ByteArray.toBlockJson(): String {
     val block = AttoBlock.fromBuffer(this.toBuffer()) ?: throw IllegalArgumentException("Invalid block")
-    return Json.encodeToString<AttoBlock>(block)
+    return json.encodeToString<AttoBlock>(block)
 }
