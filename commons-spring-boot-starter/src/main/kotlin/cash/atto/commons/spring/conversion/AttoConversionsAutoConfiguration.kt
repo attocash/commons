651a6bb0c27221d6a47021751af7d6b810cb499d
rotilho <felipe@rotilho.com>
2025-12-29 22:18:44 +0100

    Fix AttoConversionsAutoConfiguration

--- commons-spring-boot-starter/src/main/kotlin/cash/atto/commons/spring/conversion/AttoConversionsAutoConfiguration.kt
+++ commons-spring-boot-starter/src/main/kotlin/cash/atto/commons/spring/conversion/AttoConversionsAutoConfiguration.kt
@@ -1,16 +1,12 @@
 package cash.atto.commons.spring.conversion
 
-import io.r2dbc.spi.ConnectionFactory
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.beans.factory.SmartInitializingSingleton
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.support.ConfigurableConversionService
-import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
 
 @Configuration
-@ConditionalOnClass(R2dbcCustomConversions::class, ConnectionFactory::class)
 class AttoConversionsAutoConfiguration {
     @Bean
     fun attoConvertersRegistrar(conversionService: ObjectProvider<ConfigurableConversionService>) =
